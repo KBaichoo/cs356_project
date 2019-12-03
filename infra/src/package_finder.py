ac607ed19e2095b2bb3c4dc3fe40d8dea08291c3
Jayden Navarro <jaynavar@stanford.edu>
2019-12-03 11:00:14 -0800

    Fixed typo.

--- infra/src/package_finder.py
+++ infra/src/package_finder.py
@@ -283,7 +283,7 @@ if __name__ == '__main__':
    # Create github request HTTP auth.
    if args.use_github_auth:
       username = raw_input("Enter GitHub username: ")
-      passowrd = raw_input("Enter GitHub password: ")
+      password = raw_input("Enter GitHub password: ")
       github_auth = requests.auth.HTTPBasicAuth(username, password)
    else:
       github_auth = None
