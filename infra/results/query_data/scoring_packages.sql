40400b7728f2a6fb0d8e4e1e53f98854e719639a
Kevin Baichoo <kbaichoo@stanford.edu>
2019-12-13 11:04:46 -0800

    Added scoring packages + package score distribution.

---
+++ infra/results/query_data/scoring_packages.sql
@@ -0,0 +1,42 @@
+-- This only does analysis on packages with single binary + c++11 or newer
+select package_name, (smart_ptr_detected + named_cast_detected + fortified_source + stack_protector + PIE + ro_relocation + immediate_binding) as points from (
+select package_name,
+CASE
+    WHEN (
+	"detection_tool_output.unique_ptr.detected" = 'yes' OR
+	"detection_tool_output.shared_ptr.detected" = 'yes' OR
+	"detection_tool_output.weak_ptr.detected" = 'yes' OR
+	"detection_tool_output.intrusive_ptr.detected" = 'yes' OR
+	"detection_tool_output.scoped_ptr.detected" = 'yes' OR
+	"detection_tool_output.shared_array.detected" = 'yes' OR
+	"detection_tool_output.scoped_array.detected" = 'yes'
+	) THEN 1
+    ELSE 0
+END AS smart_ptr_detected,
+CASE
+    WHEN "detection_tool_output.dynamic_cast.detected" = 'yes' OR "detection_tool_output.static_cast.detected" = 'yes' OR "detection_tool_output.reinterpret_cast.detected" = 'yes' OR "detection_tool_output.const_cast.detected" = 'yes' THEN 1
+    ELSE 0
+END AS named_cast_detected,
+CASE
+	WHEN "detection_tool_output.fortify-source" LIKE '%yes%' then 1
+	ELSE 0
+END AS fortified_source,
+CASE
+	WHEN "detection_tool_output.stack-protector" LIKE '%yes%' then 1
+	ELSE 0
+END AS stack_protector,
+CASE
+	WHEN "detection_tool_output.PIE" LIKE '%yes%' then 1
+	ELSE 0
+END AS PIE,
+CASE
+	WHEN "detection_tool_output.ro-relocation" LIKE '%yes%' then 1
+	ELSE 0
+END AS ro_relocation,
+CASE
+	WHEN "detection_tool_output.immediate-binding" LIKE '%yes%' then 1
+	ELSE 0
+END AS immediate_binding
+from detection_results
+where "detection_tool_output.stack-protector" is not null AND "detection_tool_output.cpp_version" in ('c++11', 'c++14', 'c++17', 'c++2a')
+) order by points;
