a5ac97efc24743d273ed662d380784076bef97e8
Kevin Baichoo <kbaichoo@stanford.edu>
2019-12-13 10:20:39 -0800

    Added query + results for smart pointer detection in C++11 or newer.

---
+++ infra/results/query_data/smart_ptr_detected.sql
@@ -0,0 +1,26 @@
+-- Smart Pointer detection against C++11 or newer lang. versions!
+SELECT "at least one smart pointer detected", count(*) AS smart_pointer_detected
+FROM detection_results
+WHERE "detection_tool_output.cpp_version" IN ('c++11', 'c++14', 'c++17', 'c++2a') AND (
+	"detection_tool_output.unique_ptr.detected" = 'yes' OR
+	"detection_tool_output.shared_ptr.detected" = 'yes' OR
+	"detection_tool_output.weak_ptr.detected" = 'yes' OR
+	"detection_tool_output.intrusive_ptr.detected" = 'yes' OR
+	"detection_tool_output.scoped_ptr.detected" = 'yes' OR
+	"detection_tool_output.shared_array.detected" = 'yes' OR
+	"detection_tool_output.scoped_array.detected" = 'yes'
+	)
+;
+
+select "no smart pointers detected", count(*) as smart_pointer_detected
+from detection_results
+where "detection_tool_output.cpp_version" in ('c++11', 'c++14', 'c++17', 'c++2a') AND (
+	"detection_tool_output.unique_ptr.detected" = 'no' AND
+	"detection_tool_output.shared_ptr.detected" = 'no' AND
+	"detection_tool_output.weak_ptr.detected" = 'no' AND
+	"detection_tool_output.intrusive_ptr.detected" = 'no' AND
+	"detection_tool_output.scoped_ptr.detected" = 'no' AND
+	"detection_tool_output.shared_array.detected" = 'no' AND
+	"detection_tool_output.scoped_array.detected" = 'no'
+	)
+;
