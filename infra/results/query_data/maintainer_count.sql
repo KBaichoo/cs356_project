5b76be7ee97b9bf186339d1018bd17fb526efd7a
Kevin Baichoo <kbaichoo@stanford.edu>
2019-12-14 01:37:16 -0800

    Fixed column ordering.

--- infra/results/query_data/maintainer_count.sql
+++ infra/results/query_data/maintainer_count.sql
@@ -1,4 +1,4 @@
-SELECT count(*) AS num_maintainers_meeting_a_count, count_ AS num_pkgs_maintained
+SELECT  count_ AS num_pkgs_maintained, count(*) AS num_maintainers_meeting_a_count
 FROM (
 	SELECT maintainer, count(*) AS count_
 	FROM detection_results
